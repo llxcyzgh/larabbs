86eb43916b9224cf16eb829a9ae756d13417cd8d
zhujw <zhujianghan@gmail.com>
2018-09-18 14:23:50 +0800

    Generated topics

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,7 +15,9 @@ class DatabaseSeeder extends Seeder
         Model::unguard();
 
         // $this->call(UsersTableSeeder::class);
+		$this->call(TopicsTableSeeder::class);
         $this->call(UsersTableSeeder::class);
+		$this->call(TopicsTableSeeder::class);
 
         Model::reguard();
     }
